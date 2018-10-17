9f5e2b89fbb94a1af13c3ed14a67593acd1522d6
Kévin Arnoult <karnoult@users.noreply.github.com>
2018-10-17 08:23:03 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
+#install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+#enable sublime text shortcut
 ln -s "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" ~/bin/subl
 
 brew install postgres
