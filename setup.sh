2faef7738127bb44208b8c4bda7b12e34da5089f
Kévin Arnoult <karnoult@users.noreply.github.com>
2018-10-19 08:22:58 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@
 #enable sublime text shortcut
 ln -s "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" ~/bin/subl
 
+#https://www.codementor.io/engineerapart/getting-started-with-postgresql-on-mac-osx-are8jcopb
 brew install postgres
 pg_ctl -D /usr/local/var/postgres start && brew services start postgresql
