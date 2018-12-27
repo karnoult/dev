61c2339fad8da394b4fed6606f819826931ccdf1
Kévin Arnoult <karnoult@users.noreply.github.com>
2018-12-27 17:43:04 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,12 @@
 #enable sublime text shortcut
 ln -s "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" ~/bin/subl
 
+#enable Zsh Docker completion
+etc=/Applications/Docker.app/Contents/Resources/etc
+ln -s $etc/docker.zsh-completion /usr/local/share/zsh/site-functions/_docker
+ln -s $etc/docker-machine.zsh-completion /usr/local/share/zsh/site-functions/_docker-machine
+ln -s $etc/docker-compose.zsh-completion /usr/local/share/zsh/site-functions/_docker-compose
+
 #https://www.codementor.io/engineerapart/getting-started-with-postgresql-on-mac-osx-are8jcopb
 brew install postgres
 pg_ctl -D /usr/local/var/postgres start && brew services start postgresql
